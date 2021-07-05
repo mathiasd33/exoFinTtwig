1b2d9c6d427b46418eed0d77dd0104e2fa304255
mathias <mathiasd415@gmail.com>
2021-07-05 16:13:39 +0200

    Initial commit

---
+++ src/Controller/CategoriesControler.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategoriesControler extends AbstractController
+{
+
+    private $categories = [
+        1 => [
+            "title" => "Politique",
+            "content" => "Tous les articles liés à Jean Lassalle",
+            "id" => 1,
+            "published" => true,
+        ],
+        2 => [
+            "title" => "Economie",
+            "content" => "Les meilleurs tuyaux pour avoir DU FRIC",
+            "id" => 2,
+            "published" => true
+        ],
+        3 => [
+            "title" => "Securité",
+            "content" => "Attention les étrangers sont très méchants",
+            "id" => 3,
+            "published" => false
+        ],
+        4 => [
+            "title" => "Ecologie",
+            "content" => "Hummer <3",
+            "id" => 4,
+            "published" => true
+        ]
+    ];
+
+    /**
+     * @Route ("/categories", name="categories")
+     */
+
+
+}
