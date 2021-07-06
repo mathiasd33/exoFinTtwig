1350983b4e206e30a54273739ba01fc2879f4798
mathias <mathiasd415@gmail.com>
2021-07-06 15:38:59 +0200

    creat table on database

---
+++ src/Entity/article.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+
+class Article
+{
+
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue()
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $title;
+
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+}
